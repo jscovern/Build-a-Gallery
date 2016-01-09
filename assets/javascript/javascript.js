f1f63d8c70052592cb7af2ef5d8e01e2acdb0375
Jamie Scovern <jamie.scovern@gmail.com>
2016-01-08 23:49:33 -0500

    first commit

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,34 @@
+var imageArray = new Array();
+
+function previewFile() { //concept taken from http://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
+	//var myImage = $("img[data-id=" + (imageArray.length + 1) + "]");
+	//var myImage = document.querySelector('img'); //finds the img element
+	var myImage = document.querySelector('img[data-id="' + (imageArray.length + 1) + '"]'); //finds the img element
+	var myFile = document.querySelector('input[type=file][data-id="' + (imageArray.length + 1) + '"]').files[0]; //finds the input with a type of file
+	var myFileUploader = document.querySelector('input[type=file][data-id="' + (imageArray.length + 1) + '"]');
+	var reader = new FileReader();
+
+	reader.onloadend = function() {
+		myImage.src = reader.result; //sets the src of the img on the page (which is called myImage here)
+	}
+
+	if (myFile) { //if a file was uploaded
+		reader.readAsDataURL(myFile); //reads the file that was uploaded, as a url
+		$(myImage).removeClass("hidden"); //removes the hidden class from the image
+		$(myFileUploader).addClass("hidden");
+		console.log(myFileUploader);
+		imageArray.push(myImage.src); //adds in the url of the image into the imageArray
+	} else { //if a file wasn't uploaded, sets the src back to blank
+		myImage.src = "";
+	}
+}
+
+$(document).ready(function() {
+
+	$(document).on('click', '.addToGallery', function(event) {
+		event.preventDefault();
+		    $('.darkWrapper.rightGallery').append("<input type='file' onchange=previewFile() data-id=" + (imageArray.length + 1) + "><br>");
+		    $('.darkWrapper.rightGallery').append("<img class='hidden' data-id=" + (imageArray.length + 1) + " src='' height='200' width='200'>");
+	});
+
+});
