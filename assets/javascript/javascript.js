da59cf324213e4fa97db3bb8103468e361937a02
Jamie Scovern <jamie.scovern@gmail.com>
2016-01-14 18:36:28 -0500

    added in delete

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,13 +1,17 @@
 var imageArray = new Array();
+var imageInGalleryMax = 4;
 
 function previewFile() { //concept taken from http://stackoverflow.com/questions/22087076/how-to-make-a-simple-image-upload-using-javascript-html
 	//var myImage = $("img[data-id=" + (imageArray.length + 1) + "]");
 	//var myImage = document.querySelector('img'); //finds the img element
-	var myImage = document.querySelector('img[data-id="' + (imageArray.length + 1) + '"]'); //finds the img element
-	var myFile = document.querySelector('input[type=file][data-id="' + (imageArray.length + 1) + '"]').files[0]; //finds the input with a type of file
-	var myFileUploader = document.querySelector('input[type=file][data-id="' + (imageArray.length + 1) + '"]');
+	var myImage = document.querySelector('img[data-id="' + findDataID() + '"]'); //finds the img element with the correct data-band id
+	console.log("myImage: "+document.querySelector('img[data-id="' + findDataID() + '"]'));
+	var myFile = document.querySelector('input[type=file][data-id="' + findDataID() + '"]').files[0]; //finds the input with a type of file
+	console.log("myfile: "+document.querySelector('input[type=file][data-id="' + findDataID() + '"]').files[0]);
+	var myFileUploader = document.querySelector('input[type=file][data-id="' + findDataID() + '"]');
+	console.log("myFileUploader: "+document.querySelector('input[type=file][data-id="' + findDataID() + '"]'));
 	var reader = new FileReader();
-
+	console.log(reader.result);
 	reader.onloadend = function() {
 		myImage.src = reader.result; //sets the src of the img on the page (which is called myImage here)
 	}
@@ -16,26 +20,79 @@ function previewFile() { //concept taken from http://stackoverflow.com/questions
 		reader.readAsDataURL(myFile); //reads the file that was uploaded, as a url
 		$(myImage).removeClass("hidden"); //removes the hidden class from the image
 		$(myFileUploader).addClass("hidden");
-		console.log(myFileUploader);
-		imageArray.push(myImage.src); //adds in the url of the image into the imageArray
+		console.log($(myFileUploader).attr('data-id'));
+		console.log($(myFileUploader).parent().attr('data-band_id'));
+		imageArray.push($(myFileUploader).parent().attr('data-band_id') + "_" + $(myFileUploader).attr('data-id'));
+		
 	} else { //if a file wasn't uploaded, sets the src back to blank
 		myImage.src = "";
 	}
+
+	hidePlusButton($(myFileUploader).parent().attr('data-band_id'));
+}
+
+function hidePlusButton(band_id) {
+	if (numImageInGallery(band_id) === imageInGalleryMax) { //only allow 5 images in each gallery
+				$("div.plusButton[data-band_id="+band_id+"]").addClass("hidden");
+				$("div.verticalGutter[data-band_id="+band_id+"]").addClass("hidden");
+				$("div.gallery[data-band_id=" + band_id + "]").addClass("plusButtonHidden");
+			}
+}
+
+function showPlusButton(band_id) {
+	if (numImageInGallery(band_id) < imageInGalleryMax) { //only allow 5 images in each gallery
+				$("div.plusButton[data-band_id="+band_id+"]").removeClass("hidden");
+				$("div.verticalGutter[data-band_id="+band_id+"]").removeClass("hidden");
+				$("div.gallery[data-band_id=" + band_id + "]").removeClass("plusButtonHidden");
+			}
+}
+
+function findDataID() {
+	var highNum=0;
+	for( i=0; i<imageArray.length; i++) {
+		if(imageArray[i].substring(imageArray[i].search("_") + 1, imageArray[i].length) > highNum) {
+			highNum = imageArray[i].substring(imageArray[i].search("_") + 1, imageArray[i].length);
+		}
+	}
+		return highNum + 1;
+}
+
+function numImageInGallery(band_id) {
+	var imageCount = 0;
+
+	for(i=0; i<imageArray.length; i++) {
+		if(imageArray[i].substring(0,imageArray[i].search("_")) === band_id) {
+			imageCount++;
+		}
+	}
+
+	return imageCount;
+	console.log("imageCount from numImageInGallery: "+imageCount);
 }
 
 $(document).ready(function() {
 
 	$(document).on('click', '.button', function(event) {
 		event.preventDefault();
-		// console.log($(event.target).parent().parent().attr('id'));
-		// console.log($(event.target).parent().siblings());
 		var band_id = $(event.target).attr('data-band_id');
 
-		    $(".gallery[data-band_id=" + band_id + "]").append("<input type='file' onchange=previewFile() data-id=" + (imageArray.length + 1) + "><br>");
-		    $(".gallery[data-band_id=" + band_id + "]").append("<img class='hidden' data-id=" + (imageArray.length + 1) + " src='' height='200' width='200'>");
+		    $(".gallery[data-band_id=" + band_id + "]").append("<input type='file' onchange=previewFile() data-id=" + findDataID() + ">");
+		    $(".gallery[data-band_id=" + band_id + "]").append("<span class='imageWrapper' data-id=" + findDataID() + "><img class='hidden imgPreview' data-id=" + findDataID() + " data-band_id=" + band_id + " src=''><span class='deleteX' data-id=" + findDataID() + ">[x]</span></span>");
 
-		    // $('.gallery').append("<input type='file' onchange=previewFile() data-id=" + (imageArray.length + 1) + "><br>");
-		    // $('.gallery').append("<img class='hidden' data-id=" + (imageArray.length + 1) + " src='' height='200' width='200'>");
+		    $("input[type='file'][data-id=" + findDataID() +"]").click(); //auto-click the choose-file button
 	});
+
+	$(document).on('click', '.deleteX', function(event) {
+		var dataID = $(this).attr('data-id');
+		var band_id = $(this).parent().parent().attr('data-band_id');
+		console.log("dataID: "+dataID);
+		var arrayPosition = imageArray.indexOf(band_id+"_"+dataID);
+		imageArray.splice(arrayPosition,1);
+		$(".imageWrapper[data-id=" + dataID + "]").remove();
+		$("input[type=file][data-id=" + dataID + "]").remove();
+		console.log("deleteX band_id: "+band_id);
+		showPlusButton(band_id);
+	});
+
 
 });
